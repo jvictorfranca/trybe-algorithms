24ea491a8c8063ca77e700f015ce72e6a8bdc41d
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2022-04-20 14:42:49 -0300

    faz logica de palindromo recursivo

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,16 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+
+
+
+word = 'helleh'
+
+def is_palindrome(word):
+  string_list = list(word)
+  if len(string_list) == 1:
+    return True
+  if len(string_list) == 2:
+    return string_list[0] == string_list[1]
+  return string_list[0] == string_list[-1] and is_palindrome("".join(string_list[1:-1]))
+
+print(is_palindrome(word))
