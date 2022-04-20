d1112950d629a5b08546df601e5ec42c6407a025
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2022-04-20 14:54:13 -0300

    faz o requisito do palindromo acho que é o 2, não sei para que precisa de argumentos mas tá né"

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,19 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
-
-
-word = 'helleh'
-
-
-def is_palindrome(word):
     string_list = list(word)
+    if len(string_list) == 0:
+        return False
     if len(string_list) == 1:
         return True
     if len(string_list) == 2:
         return string_list[0] == string_list[1]
-    return string_list[0] == string_list[-1] and is_palindrome(
-      "".join(string_list[1:-1])
+    return string_list[0] == string_list[-1] and is_palindrome_recursive(
+      "".join(string_list[1:-1]), 0, 0
       )
-
-
-print(is_palindrome(word))
