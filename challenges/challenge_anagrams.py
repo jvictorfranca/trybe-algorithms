dbe32da908b3557170a3f8b4a2dae9fe22775406
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2022-04-20 14:48:26 -0300

    arruma linter

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -2,10 +2,11 @@ def merge_sort(word):
     array = list(word)
     if len(array) <= 1:
         return array
-        
+
     mid = len(array) // 2
     left, right = merge_sort(array[:mid]), merge_sort(array[mid:])
     return merge(left, right, array.copy())
+
 
 def merge(left, right, merged):
 
@@ -26,6 +27,7 @@ def merge(left, right, merged):
     for right_cursor in range(right_cursor, len(right)):
         merged[left_cursor + right_cursor] = right[right_cursor]
     return "".join(merged)
+
 
 def is_anagram(first_string, second_string):
     first = first_string.lower()
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -2,15 +2,18 @@ def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
 
 
-
 word = 'helleh'
 
+
 def is_palindrome(word):
-  string_list = list(word)
-  if len(string_list) == 1:
-    return True
-  if len(string_list) == 2:
-    return string_list[0] == string_list[1]
-  return string_list[0] == string_list[-1] and is_palindrome("".join(string_list[1:-1]))
+    string_list = list(word)
+    if len(string_list) == 1:
+        return True
+    if len(string_list) == 2:
+        return string_list[0] == string_list[1]
+    return string_list[0] == string_list[-1] and is_palindrome(
+      "".join(string_list[1:-1])
+      )
+
 
 print(is_palindrome(word))
